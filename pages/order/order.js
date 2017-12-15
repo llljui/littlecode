3de55ba36c0dd9c80d4b70db6011677a45771e72
lixijiang <872013164@qq.com>
2017-12-15 17:46:24 +0800

    s

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,6 +1,102 @@
 Page({
   data: {
-    logs: []
+    logs: [],
+    status:'状态',
+    carname:'浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+    odernumber:'121212121',
+    borrow_time:'2017-10-10',
+    back_time:'2017-08-09',
+    cartype:'奔驰-2016',
+    price:'1000',
+    order_list: [{
+      status: '状态',
+      carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+      odernumber: '121212121',
+      borrow_time: '2017-10-10',
+      back_time: '2017-08-09',
+      cartype: '奔驰-2016',
+      price: '1000'
+    }, {
+      status: '状态',
+      carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+      odernumber: '121212121',
+      borrow_time: '2017-10-10',
+      back_time: '2017-08-09',
+      cartype: '奔驰-2016',
+      price: '1000'
+      }, {
+        status: '状态',
+        carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+        odernumber: '121212121',
+        borrow_time: '2017-10-10',
+        back_time: '2017-08-09',
+        cartype: '奔驰-2016',
+        price: '1000'
+    }, {
+      status: '状态',
+      carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+      odernumber: '121212121',
+      borrow_time: '2017-10-10',
+      back_time: '2017-08-09',
+      cartype: '奔驰-2016',
+      price: '1000'
+      }, {
+        status: '状态',
+        carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+        odernumber: '121212121',
+        borrow_time: '2017-10-10',
+        back_time: '2017-08-09',
+        cartype: '奔驰-2016',
+        price: '1000'
+    }, {
+      status: '状态',
+      carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+      odernumber: '121212121',
+      borrow_time: '2017-10-10',
+      back_time: '2017-08-09',
+      cartype: '奔驰-2016',
+      price: '1000'
+      }, {
+        status: '状态',
+        carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+        odernumber: '121212121',
+        borrow_time: '2017-10-10',
+        back_time: '2017-08-09',
+        cartype: '奔驰-2016',
+        price: '1000'
+    }, {
+      status: '状态',
+      carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+      odernumber: '121212121',
+      borrow_time: '2017-10-10',
+      back_time: '2017-08-09',
+      cartype: '奔驰-2016',
+      price: '1000'
+      }, {
+        status: '状态',
+        carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+        odernumber: '121212121',
+        borrow_time: '2017-10-10',
+        back_time: '2017-08-09',
+        cartype: '奔驰-2016',
+        price: '1000'
+    }, {
+      status: '状态',
+      carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+      odernumber: '121212121',
+      borrow_time: '2017-10-10',
+      back_time: '2017-08-09',
+      cartype: '奔驰-2016',
+      price: '1000'
+      }, {
+        status: '状态',
+        carname: '浙江大地是的是的是的是是的是的四点开始看的还是抠脚大汉授课计划',
+        odernumber: '121212121',
+        borrow_time: '2017-10-10',
+        back_time: '2017-08-09',
+        cartype: '奔驰-2016',
+        price: '1000'
+      }]
   },
   onLoad: function () {
    
